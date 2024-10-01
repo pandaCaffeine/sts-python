d425145e07e597a9a290da4cae427ac390f543a0
Nikita Sakhno <nikita.sakhno@waveaccess.global>
2024-10-02 00:52:45 +0500

    finish main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from io import BytesIO
 from typing import Annotated, Generator
 
 import uvicorn
+from Crypto.SelfTest.Cipher.test_CBC import file_name
 from PIL import Image
 from fastapi import FastAPI, Header, Response, status, HTTPException
 from fastapi.params import Depends
@@ -12,7 +13,7 @@ from starlette.responses import StreamingResponse
 from urllib3 import BaseHTTPResponse
 
 from config import app_settings, AppSettings
-from storage_client import StorageClient, S3StorageClient
+from storage_client import StorageClient, S3StorageClient, StorageResponse
 
 HEADER_ETAG = "Etag"
 HEADER_LEN = "Content-Length"
@@ -48,6 +49,7 @@ def stream_response(http_response: BaseHTTPResponse) -> Generator:
             yield chunk
     finally:
         http_response.close()
+        http_response.release_conn()
 
 
 def stream_bytesio(data: BytesIO) -> Generator:
@@ -82,48 +84,34 @@ def read_file_to_memory(http_response: BaseHTTPResponse) -> BytesIO:
 
 @app.get("/{bucket}/{filename}")
 def get_thumbnail(bucket: str, filename: str,
-                  s3client: Annotated[Minio, Depends(minio_client)],
+                  s3client: Annotated[StorageClient, Depends(resolve_storage_client)],
                   etag: Annotated[str | None, Header(alias="If-None-Match")] = None):
-    s3resp: BaseHTTPResponse | None = None
-
-    thumbnail_stat = test_file_in_bucket(s3client, bucket, filename)
+    thumbnail_stat = s3client.get_file_stat(bucket, filename)
     if thumbnail_stat:
         if etag and thumbnail_stat.etag == etag:
             headers = {HEADER_ETAG: etag}
             return Response(status_code=status.HTTP_304_NOT_MODIFIED, headers=headers)
 
-        try:
-            s3resp = s3client.get_object(bucket_name=bucket, object_name=filename)
+        r = s3client.open_stream(bucket, filename)
+        with r:
             headers = {HEADER_ETAG: thumbnail_stat.etag, HEADER_LEN: str(thumbnail_stat.size)}
-            return StreamingResponse(stream_response(s3resp), media_type=thumbnail_stat.content_type,
-                                     headers=headers)
-        finally:
-            if s3resp:
-                s3resp.release_conn()
+            return StreamingResponse(r.read_to_end(), media_type=thumbnail_stat.content_type, headers=headers)
 
-    source_file_stat = test_file_in_bucket(s3client, app_settings.source_bucket, filename)
+    source_file_stat = s3client.get_file_stat(app_settings.source_bucket, file_name)
     if not source_file_stat:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="File not found")
 
-    try:
-        s3resp = s3client.get_object(bucket_name=app_settings.source_bucket, object_name=filename)
-        size = (float(app_settings.buckets[bucket].width), float(app_settings.buckets[bucket].height))
-        with read_file_to_memory(s3resp) as source_data:
-            with Image.open(source_data) as im:
-                im.thumbnail(size)
-                thumbnail_data = BytesIO()
-                im.save(thumbnail_data, format=FORMAT_JPEG)
+    size = (float(app_settings.buckets[bucket].width), float(app_settings.buckets[bucket].height))
+    image_mem = s3client.load_file(bucket, filename)
+    with image_mem:
+        with Image.open(image_mem) as im:
+            im.thumbnail(size)
+            thumbnail_data = BytesIO()
+            im.save(thumbnail_data, format=FORMAT_JPEG)
 
-        thumbnail_data_size = thumbnail_data.tell()
-        thumbnail_data.seek(0, os.SEEK_SET)
-        put_result = s3client.put_object(bucket_name=bucket, object_name=filename, data=thumbnail_data,
-                                         length=thumbnail_data_size, content_type=CONTENT_TYPE_JPEG)
-        headers = {HEADER_ETAG: put_result.etag, HEADER_LEN: str(thumbnail_data_size)}
-        return StreamingResponse(stream_bytesio(thumbnail_data), media_type=CONTENT_TYPE_JPEG, headers=headers)
-    finally:
-        if s3resp:
-            s3resp.close()
-            s3resp.release_conn()
+    put_result = s3client.put_file(bucket, filename, thumbnail_data, content_type=CONTENT_TYPE_JPEG)
+    headers = {HEADER_ETAG: put_result.etag, HEADER_LEN: str(put_result.size)}
+    return StreamingResponse(stream_bytesio(thumbnail_data), media_type=CONTENT_TYPE_JPEG, headers=headers)
 
 
 if __name__ == "__main__":
