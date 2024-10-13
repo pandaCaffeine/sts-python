d5aa22ac83623efccadb9cfb1060b4d17ca33c65
Nikita Sakhno <nikita.sakhno@waveaccess.global>
2024-10-13 21:21:36 +0500

    support for different image formats were added

---
+++ image_processor.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from io import BytesIO
+
+import PIL
+from PIL import Image
+
+
+@dataclass(frozen=True)
+class ImageData:
+    mime_type: str
+    error: Exception | None = None
+    data: BytesIO | None = None
+
+
+def resize_image(data: BytesIO, width: float, height: float) -> ImageData:
+    assert data is not None, "data cannot be None"
+    assert width > 0, "width must be greater than 0"
+    assert height > 0, "height must be greater than 0"
+
+    with data:
+        try:
+            with Image.open(data) as im:
+                mime_type = im.get_format_mimetype()
+
+                im.thumbnail((width, height))
+                result = BytesIO()
+                im.save(result, im.format)
+                return ImageData(mime_type=mime_type, error=None, data=result)
+        except (PIL.UnidentifiedImageError, ValueError, TypeError, Exception) as e:
+            return ImageData(mime_type=mime_type, error=e, data=None)
