75123db73e3800793de80f78dd5f99f051b29c07
Nikita Sakhno <adream74@gmail.com>
2024-10-23 23:43:02 +0500

    🏥 Health check (#6)
    * health check service and routes were added
    
    * health check endpoint was improved

---
+++ app/healthcheck/service.py
@@ -0,0 +1,15 @@
+from app.models import BucketsInfo
+
+
+class HealthCheckService:
+    __buckets_info: BucketsInfo = None
+
+    def set_buckets_info(self, buckets_info: BucketsInfo):
+        assert buckets_info is not None, "buckets_info is required"
+        assert self.__buckets_info is None, "buckets_info is readonly"
+
+        self.__buckets_info = buckets_info
+
+    @property
+    def bucket_info(self) -> BucketsInfo:
+        return self.__buckets_info
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import uvicorn
 from loguru import logger
 
 from app.dependencies import get_app_settings
+from app.healthcheck.dependencies import get_health_check_service
 from app.images.buckets_service import BucketsService
 from app.images.dependencies import get_minio_client, get_storage_client
 from app.main import web_app
@@ -26,8 +27,11 @@ def __start_app():
     minio = get_minio_client()
     storage_client = get_storage_client(minio)
 
-    buckets_service = BucketsService(app_settings, storage_client)
-    buckets_service.create_buckets()
+    buckets_service = BucketsService(app_settings, storage_client, l)
+    buckets_info = buckets_service.create_buckets()
+
+    hc_service = get_health_check_service()
+    hc_service.set_buckets_info(buckets_info)
 
     l.info("Starting web host")
     uvicorn.run(web_app, host="0.0.0.0", port=8000)
