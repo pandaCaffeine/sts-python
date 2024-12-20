62af13479a5efadb8ce92a24c46b203288b48bf0
Nikita Sakhno <nikita.sakhno@waveaccess.ru>
2024-12-21 02:10:31 +0500

    recreate migration

--- src/sts/stats/models.py
+++ src/sts/stats/models.py
@@ -24,5 +24,4 @@ class RequestStat(BaseModel):
     hits: Mapped[int] = mapped_column(default=0)
     update_dt: Mapped[datetime.datetime] = mapped_column(default=func.now)
     errors: Mapped[int] = mapped_column(default=0)
-    r_priority: Mapped[RequestPriorityEnum] = mapped_column(sqlalchemy.Enum(RequestPriorityEnum),
-                                                          nullable=False, default=RequestPriorityEnum.LOW)
+    r_priority: Mapped[RequestPriorityEnum] = mapped_column(default=RequestPriorityEnum.LOW)
