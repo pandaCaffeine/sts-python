8f6ccdc01adfccbecc0fc12edc0d817e1bcf7d43
Nikita Sakhno <nikita.sakhno@waveaccess.ru>
2025-01-01 22:38:24 +0500

    config was fixed; main.py was renamed to __main__.py;

--- src/sts/__init__.py
+++ src/sts/__init__.py
@@ -1,12 +1,3 @@
 __author__ = "Nikita Sakhno"
 __license__ = "MIT License"
 __version__ = "1.1.0"
-
-from fastapi import FastAPI
-
-from sts.healthcheck.routes import hc_route
-from sts.images.routes import images_router
-
-web_app = FastAPI()
-web_app.include_router(images_router)
-web_app.include_router(hc_route)
--- src/sts/__main__.py
+++ src/sts/__main__.py
@@ -7,7 +7,9 @@ from sts.config import app_settings
 from sts.healthcheck.dependencies import get_health_check_service
 from sts.images.buckets_service import BucketsService
 from sts.images.dependencies import get_minio_client, get_storage_client
-from sts import web_app
+from sts.images.routes import images_router
+from sts.healthcheck.routes import hc_route
+from fastapi import FastAPI
 from sts import __version__
 
 
@@ -17,6 +19,13 @@ def __configure_logger():
     logger.add(sys.stderr, level="ERROR", format=app_settings.log_fmt)
     logger.add("logs/log_{time}.log", level=app_settings.log_level, retention="10 days",
                format=app_settings.log_fmt)
+
+
+def __create_fastapi_app() -> FastAPI:
+    result = FastAPI()
+    result.include_router(images_router)
+    result.include_router(hc_route)
+    return result
 
 
 def __start_app():
@@ -32,11 +41,11 @@ def __start_app():
     hc_service = get_health_check_service()
     hc_service.set_buckets_info(buckets_info)
 
+    web_app = __create_fastapi_app()
     l.info("Starting web host")
     uvicorn.run(web_app, host="0.0.0.0", port=80, proxy_headers=True)
 
 
 if __name__ == "__main__":
     print(f"sts v{__version__}")
-
     __start_app()
