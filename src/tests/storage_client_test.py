3f27b129aa9d17a0662894ac7b8e620c3d20bb85
Nikita Sakhno <nikita.sakhno@waveaccess.ru>
2025-01-12 22:23:00 +0500

    storage_client create_dir tests were added

--- src/tests/storage_client_test.py
+++ src/tests/storage_client_test.py
@@ -1,4 +1,5 @@
-from unittest.mock import create_autospec
+import unittest.mock
+from unittest.mock import create_autospec, Mock
 
 import pytest
 from minio import Minio, S3Error
@@ -10,12 +11,12 @@ expected_content_length = '1024'
 expected_content_type = 'image/png'
 expected_etag = '53e2a123b39d45339b7d6f14b99292b8'
 
-fake_response = HTTPResponse(body="", headers={
+fake_response = HTTPResponse(body='', headers={
     'content-length': expected_content_length,
     'content-type': expected_content_type,
     'etag': f'"{expected_etag}"'
-}, status=200, version=1, version_string="1", reason=None,
-                             decode_content=False, request_url="http://minio/images/icon.png")
+}, status=200, version=1, version_string='1', reason=None,
+                             decode_content=False, request_url='http://minio/images/icon.png')
 
 
 def test_open_stream_successful():
@@ -38,7 +39,7 @@ def test_open_stream_successful():
 def test_open_stream_returns_none_when_s3_error():
     # arrange
     minio_mock = create_autospec(Minio)
-    minio_mock.get_object.side_effect = S3Error("unit-test", "unit test error", None, None, None,
+    minio_mock.get_object.side_effect = S3Error('unit-test', 'unit test error', None, None, None,
                                                 response=fake_response)
 
     storage_client = S3StorageClient(minio_mock)
@@ -59,3 +60,30 @@ def test_open_stream_returns_none_when_exception():
     # act & assert
     with pytest.raises(ValueError):
         storage_client.open_stream('images', 'icon.png')
+
+def test_try_create_dir_does_nothing_if_bucket_exits():
+    # arrange
+    minio_mock = create_autospec(Minio)
+    minio_mock.bucket_exists.return_value = True
+    storage_client = S3StorageClient(minio_mock)
+
+    # act
+    result = storage_client.try_create_dir('test', 30)
+
+    # assert
+    assert not result
+
+def test_try_create_dir_successful():
+    # arrange
+    minio_mock = create_autospec(Minio)
+    minio_mock.bucket_exists.return_value = False
+    minio_mock.make_bucket = Mock(return_value=None)
+    storage_client = S3StorageClient(minio_mock)
+
+    # act
+    result = storage_client.try_create_dir('test', 30)
+
+    # assert
+    assert result
+    minio_mock.make_bucket.assert_called_with('test')
+    minio_mock.set_bucket_lifecycle.assert_called_once_with('test', unittest.mock.ANY)
