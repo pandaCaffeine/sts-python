8a65dc402c91467fa897ce880c0bddb0559c1b2c
Nikita Sakhno <adream74@gmail.com>
2025-01-26 12:11:23 +0500

    more tests

--- tests/app_settings_test.py
+++ tests/app_settings_test.py
@@ -1,4 +1,4 @@
-from sts.config import get_app_settings
+from sts.config import get_app_settings, get_buckets_map, ImageSize
 
 
 def test_read():
@@ -46,3 +46,49 @@ def test_read():
     assert bucket_settings.life_time_days == 10
     assert bucket_settings.size.w == 100
     assert bucket_settings.size.h == 100
+
+def test_buckets_map():
+    # arrange & act
+    buckets_map = get_buckets_map()
+
+    # assert
+    assert buckets_map
+    assert buckets_map.source_bucket == 'pictures'
+    assert buckets_map.all_source_buckets == {'pictures', 'images'}
+    assert buckets_map.alias_map == { 'small': 'thumbnail-small', 'medium': 'thumbnail-medium' }
+
+    # assert source bucket
+    bucket_settings = buckets_map.buckets['pictures']
+    assert bucket_settings.source_bucket == 'pictures'
+    assert bucket_settings.size == ImageSize(100, 100)
+
+    # assert thumbnail-small bucket
+    bucket_settings = buckets_map.buckets['thumbnail-small']
+    assert bucket_settings.alias == 'small'
+    assert bucket_settings.source_bucket == 'pictures'
+    assert bucket_settings.life_time_days == 30
+    assert bucket_settings.size.w == 300
+    assert bucket_settings.size.h == 300
+
+    # assert thumbnail-medium
+    bucket_settings = buckets_map.buckets['thumbnail-medium']
+    assert bucket_settings
+    assert bucket_settings.alias == 'medium'
+    assert bucket_settings.source_bucket == 'images'
+    assert bucket_settings.life_time_days == 30
+    assert bucket_settings.size.w == 500
+    assert bucket_settings.size.h == 500
+
+    # assert thumbnail
+    bucket_settings = buckets_map.buckets['thumbnail']
+    assert bucket_settings
+    assert not bucket_settings.alias
+    assert bucket_settings.source_bucket == 'pictures'
+    assert bucket_settings.life_time_days == 10
+    assert bucket_settings.size.w == 100
+    assert bucket_settings.size.h == 100
+
+
+
+
+
