2007e5a17262a4b759bee02f3f018a33c0c6a552
nikita <nikita.sakhno@waveaccess.global>
2025-04-27 22:18:46 +0500

    runtime error was fixed

--- src/sts/healthcheck/reader.py
+++ src/sts/healthcheck/reader.py
@@ -5,8 +5,8 @@ from sts.models.bucket import BucketsInfo
 class HealthCheckReader(ABC):
     """ Service to read health check information """
 
-    @abstractmethod
     @property
+    @abstractmethod
     def bucket_info(self) -> BucketsInfo:
         """ Returns buckets info set before """
         pass
