46a5fd2f1f8a0bfd61badfa483b6bd8ce9afa314
nikita <nikita.sakhno@waveaccess.global>
2025-04-27 22:18:31 +0500

    fix routes - resolve a service from container

--- src/sts/api/images.py
+++ src/sts/api/images.py
@@ -1,5 +1,6 @@
 from typing import Annotated
 
+from dishka import FromDishka
 from dishka.integrations.fastapi import DishkaSyncRoute
 from fastapi import APIRouter, Header, Response
 
@@ -12,13 +13,13 @@ _EtagDep = Annotated[str | None, Header(alias="If-None-Match")]
 
 @images_router.get("/{bucket}/{file_name}")
 def get_thumbnail(bucket: str, file_name: str,
-                  thumbnail_service: ThumbnailService,
+                  thumbnail_service: FromDishka[ThumbnailService],
                   etag: _EtagDep = None) -> Response:
     return thumbnail_service.get_thumbnail(bucket, file_name, etag)
 
 
 @images_router.get("/{bucket}/{file_name}/{alias}")
 def get_thumbnail_by_alias(bucket: str, file_name: str, alias: str,
-                           thumbnail_service: ThumbnailService,
+                           thumbnail_service: FromDishka[ThumbnailService],
                            etag: _EtagDep = None) -> Response:
     return thumbnail_service.get_thumbnail_by_alias(bucket, file_name, alias, etag)
